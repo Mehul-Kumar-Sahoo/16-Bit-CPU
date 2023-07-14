3135d7de8dd740fc389ec2b60241b0df6891b364
Mehul Kumar Sahoo <93527557+Mehul-Kumar-Sahoo@users.noreply.github.com>
2023-07-14 18:21:11 +0530

    Fixing Jump Instructions

--- Verilog files/PC.v
+++ Verilog files/PC.v
@@ -20,13 +20,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module PC(addr_ins,clk,rst,pc_hold);
-    input clk,rst,pc_hold;
+module PC(addr_ins,clk,rst,pc_load,jump_ins);
+    input clk,rst,pc_load;
+    input [9:0] jump_ins;
     output reg [9:0] addr_ins;
 
-    always @(negedge clk or posedge rst) begin
+    always @(negedge clk or posedge rst or posedge pc_load) begin
         if(rst) addr_ins <= 10'b0;
-        else if(pc_hold) addr_ins <= addr_ins;
+        else if(pc_load) addr_ins <= jump_ins;
         else addr_ins <= addr_ins + 10'b1;
     end
 endmodule
